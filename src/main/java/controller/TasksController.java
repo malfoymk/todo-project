ca89bbe693e75f49a2add9a1c238abfd7dc8c4b3
Patrick Lopes <lopes.patrick4@gmail.com>
2024-04-23 10:43:34 -0300

    New Controllers for any Tasks

--- src/main/java/controller/TasksController.java
+++ src/main/java/controller/TasksController.java
@@ -14,6 +14,22 @@ public class TasksController {
         this.tasksService = tasksService;
     }
 
+    @GetMapping("/{id}")
+    public Tasks getTasks(@PathVariable Long id) {
+        return tasksService.getTasks(id);
+    }
+    
+    @PostMapping
+    public void createTasks (@RequestBody Tasks tasks) {
+        tasksService.createTasks(tasks);
+    }
+
+    @PutMapping("/{id}")
+    public void UpdateTasks(@PathVariable Long id, @RequestBody Tasks tasks) {
+        tasksService.updateTasks(id, tasks);
+    }
+
+
     @DeleteMapping("/{id}")
     public static deleteTask(@PathVariable Long id) {
         tasksService.deleteTask(id);
