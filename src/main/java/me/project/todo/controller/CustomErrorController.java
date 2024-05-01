58e49696bb409237b138cb33c0a606f9499e32cd
Patrick Lopes <lopes.patrick4@gmail.com>
2024-05-01 20:23:22 -0300

    Add CustomError Fix

--- src/main/java/me/project/todo/controller/CustomErrorController.java
+++ src/main/java/me/project/todo/controller/CustomErrorController.java
@@ -3,18 +3,16 @@ package me.project.todo.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
-    public String handleError() {
-        // Aqui você pode personalizar a página de erro 404 ou redirecionar para uma página específica
-        return "error"; // Por exemplo, "error" pode ser o nome de uma página HTML em seus recursos estáticos
-    }
+    private static final String PATH = "/error";
 
-    @Override
-    public String getErrorPath() {
-        return "/error";
+    @RequestMapping(value = PATH)
+    @ResponseBody
+    public String error() {
+        return "Error handling";
     }
-    }
+}
