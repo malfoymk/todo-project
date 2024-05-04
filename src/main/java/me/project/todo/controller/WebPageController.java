6ce850ec281df1db18c4699c0e52bcf1f0421b66
Patrick Lopes <lopes.patrick4@gmail.com>
2024-05-03 22:17:08 -0300

    chore: Configure Thymeleaf for HTML templates and fix file paths

--- src/main/java/me/project/todo/controller/WebPageController.java
+++ src/main/java/me/project/todo/controller/WebPageController.java
@@ -1,13 +1,15 @@
 package me.project.todo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebPageController {
 
     @GetMapping("/home")
-    public String index() {
-        return "index.html"; // Retorna a página index.html quando acessar /home
+    public String index(Model model) {
+        model.addAttribute("nomeDoAtributo", "Valor do Atributo");
+        return "index.html"; // Retorna o nome do arquivo HTML sem extensão
     }
 }
