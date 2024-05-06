edcb876017202d9fc7f2e1d43646973dceaa4f3e
Patrick Lopes <lopes.patrick4@gmail.com>
2024-05-06 10:28:52 -0300

    chore: Add Swagger annotations to controllers

--- src/main/java/me/project/todo/controller/CustomErrorController.java
+++ src/main/java/me/project/todo/controller/CustomErrorController.java
@@ -4,11 +4,16 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.annotations.Api;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Api(tags = "End Points Error")
 @Controller
 public class CustomErrorController implements ErrorController {
 
     private static final String ERROR_PATH = "/error";
-
+    
+    @ApiIgnore
     @RequestMapping(value = ERROR_PATH)
     public String error() {
         return "error.html"; // Retorna o nome da página HTML de erro
--- src/main/java/me/project/todo/controller/WebPageController.java
+++ src/main/java/me/project/todo/controller/WebPageController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.swagger.annotations.Api;
+
+@Api(tags = "End Points Web Page")
 @Controller
 public class WebPageController {
 
