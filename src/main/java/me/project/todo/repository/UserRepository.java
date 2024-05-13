6589c9e4ca9927361c9e535a5c8aa6cfdc087b85
Patrick Lopes <lopes.patrick4@gmail.com>
2024-05-13 09:54:33 -0300

    Feat: Email

--- src/main/java/me/project/todo/repository/UserRepository.java
+++ src/main/java/me/project/todo/repository/UserRepository.java
@@ -8,4 +8,6 @@ import me.project.todo.model.Account;
 @Repository
 public interface UserRepository extends JpaRepository<Account, Long> {
     Account findByUsername(String username);
+    Account findByEmail(String email);
 }
+
